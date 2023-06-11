b17496b11031a0e104c45cd3f5c74ea86d4e086c
sweta-126 <78642144+sweta-126@users.noreply.github.com>
2023-06-11 21:25:20 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ dotenv.config();
 const PORT = process.env.PORT || 8800;
 
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://jade-scone-8c6123.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
